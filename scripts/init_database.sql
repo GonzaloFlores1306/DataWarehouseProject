ad21b1ce72342f7edc60bd5e50f4e9f02f5a9020
Gonzalo Flores <gonzalucho2004@hotmail.com>
2025-08-16 22:44:33 +0630

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,19 @@
+USE master;
+GO;
 
+--Drop and Recreate the Database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK INMEDIATE;
+  DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+USE DataWarehouse;
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
